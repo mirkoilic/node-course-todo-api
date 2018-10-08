2e8716de7acca22abc26b9fd2bd66d2bf5dc7c3e
Mirko Ilic <mirko.shmory@gmail.com>
2018-10-08 15:26:26 +0200

    Practice mongoose queries. Add GET /todos/id

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
+var {ObjectID} = require('mongodb');
 var app = express();
 
 app.use(bodyParser.json());
@@ -28,6 +28,21 @@ app.get('/todos', (req, res) => {
     });
 });
 
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 app.listen(3000, ()=>{
     console.log('Started on port 3000');
 })
